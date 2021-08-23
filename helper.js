a8aa9eb94a92943b93d6d2c881033c16f2fef911
macuser <macuser@macusers-MacBook-Pro.local>
2021-08-23 22:27:58 +0800

    Initialisation

---
+++ helper.js
@@ -0,0 +1,43 @@
+const categories = [
+  "Course Description",
+  "Partnership",
+  "Linkage and Integration",
+  "Meaningful Service activities",
+  "Reflection",
+  "Assessment for learning",
+  "Support System",
+  "Uncertainty avoidance",
+  "Communities of practice",
+  "Evaluation and improvement",
+  "Challanges"
+];
+
+const subElements = [
+  "Transactional partnership",
+  "Transformational partnership",
+  "Sustainable partnership",
+  "Meaningful service (from the perspective of students",
+  "Direct or indirect service",
+  "Short-term intensive service",
+  "20-40 hours regular service",
+  "Student-led service",
+  "Understanding Service-Learning",
+  "Writing to reflect: journal, reflective essay, term paper",
+  "Speaking to reflect: consultation, presentation",
+  "Course "
+];
+
+const test = [];
+
+const universityList = [
+  "CityU",
+  "CUHK",
+  "EdUHK",
+  "HKBU",
+  "HKU",
+  "HKUST",
+  "LU",
+  "OUHK",
+  "PolyU"
+];
+const deptList = ["Department of Visual Arts"];
